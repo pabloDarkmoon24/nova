465626b40c2272a0f9199ba931de29d49bae9727
PabloPerez <vtrpablo4@gmail.com>
2025-07-17 16:04:59 -0500

    commit inicial

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import ghPages from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: '/nova/'
 })
